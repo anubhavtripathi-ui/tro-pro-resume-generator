349a9a78e7e133b6b693aefad5c37b33ca0522fb
anubhavtripathi-ui <anubhav.lucky2000@gmail.com>
2026-06-08 17:38:29 +0530

    Create cover_letter_generator.py

---
+++ cover_letter_generator.py
@@ -0,0 +1,12 @@
+def generate_cover_letter(
+    candidate_name,
+    company_name,
+    role_name,
+    content,
+):
+    return {
+        "candidate_name": candidate_name,
+        "company_name": company_name,
+        "role_name": role_name,
+        "content": content,
+    }
